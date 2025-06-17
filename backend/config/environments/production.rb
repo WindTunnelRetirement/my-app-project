e0fca3ef2fd47ce78e0289d5bcba49ceb6a27a11
WindTunnelRetirement <ishii@dentec.co.jp>
2025-06-17 17:14:43 +0900

    README更新

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
-  config.eager_load = false
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
