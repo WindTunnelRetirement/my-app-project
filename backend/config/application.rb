5e387a5c248e43b893239cb777732b6594c9fbba
WindTunnelRetirement <ishii@dentec.co.jp>
2025-07-08 10:55:03 +0900

    ログイン機能実装

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -28,5 +28,10 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths << Rails.root.join('app', 'lib')
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.time_zone = 'Tokyo'
   end
 end
