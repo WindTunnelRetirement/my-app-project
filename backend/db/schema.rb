b6ad66c2e4cf5aa296e1e389ae2d9b1bca789fab
WindTunnelRetirement <ishii@dentec.co.jp>
2025-07-09 10:29:54 +0900

    DB修正

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,25 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_17_023735) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_05_041812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.boolean "done"
+    t.string "title", null: false
+    t.boolean "done", default: false
+    t.integer "priority", default: 1
+    t.string "category", default: "general"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "priority"
-    t.string "category"
+    t.index ["user_id", "created_at"], name: "index_tasks_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "name"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
